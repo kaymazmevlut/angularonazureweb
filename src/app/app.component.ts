6402619a7771ef8f2f5a0ee39e4d22a141d3e1d7
mevlut <mevlutkaymaz@gmail.com>
2022-11-20 23:42:26 -0600

    updated api path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,5 +19,28 @@ export class AppComponent implements OnInit {
       console.log(x);
       this.title=x.greeting;
     })
+    this.getUserInfo();
   }
+
+  getUserInfo() {
+    const response = this.http.get<userDetails>('/.auth/me');
+    console.log(response);
+  }
+}
+
+interface userDetails {
+  "identityProvider": string,
+  "userId": string,
+  "userDetails": string,
+  "userRoles": userRoles,
+  "claims": claims[]
+}
+enum userRoles {
+  anonymous="ANONYMOUS",
+  authenticated="AUTHENTICATED"
+}
+
+interface claims{
+  "typ": string,
+  "val": string
 }
